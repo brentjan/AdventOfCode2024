84e003f8c530bd49f31989581edfe7b85348c678
brentjan <brent.janssens01@gmail.com>
2024-12-04 14:27:25 +0100

    dag 4

---
+++ advent4.py
@@ -0,0 +1,47 @@
+def is_inbound(x, y, n, m):
+    return 0 <= x < n and 0 <= y < m
+
+
+def get_strings_part1(x, y, lines, n, m):
+    strings = []
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx != 0 or dy != 0:
+                string = lines[x][y]
+                for factor in range(1, 4):
+
+                    new_x, new_y = x+dx*factor, y+dy*factor
+                    string += lines[new_x][new_y] if is_inbound(new_x, new_y, n, m) else ""
+                
+                if len(string) == 4:
+                    strings.append(string)
+
+    return sum([string == 'XMAS' for string in strings])
+
+
+def get_strings(x, y, lines, n, m):
+    if not (0 < x < n - 1 and 0 < y < m - 1):
+        return 0
+    
+    diagonal_1 = lines[x-1][y-1] + lines[x][y] + lines[x+1][y+1]
+    diagonal_2 = lines[x-1][y+1] + lines[x][y] + lines[x+1][y-1]
+
+    if diagonal_1 in ['MAS', 'SAM'] and diagonal_2 in ['MAS', 'SAM']:
+        return 1
+    else:
+        return 0
+            
+
+# Bepaal oplossing (part 2)
+name = "data/advent4.in"
+total = 0
+
+with open(name) as file:
+    lines = [line.rstrip() for line in file]
+    n, m = len(lines[0]), len(lines)
+
+    for y in range(m):
+        for x in range(n):
+            total += get_strings(x, y, lines, n, m)
+
+print(total)
